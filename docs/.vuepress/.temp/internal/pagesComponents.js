97a7fb3a6dccb8010bbc6f60c290f94fdf741c14
shanzha <liuzhonghu521@gmail.com>
2022-08-10 21:48:52 +0800

    fixed:完善新页面

--- docs/.vuepress/.temp/internal/pagesComponents.js
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -5,8 +5,8 @@ export const pagesComponents = {
   "v-79fdd481": defineAsyncComponent(() => import(/* webpackChunkName: "v-79fdd481" */"D:/vuepress-starter/docs/.vuepress/.temp/pages/home.html.vue")),
   // path: /
   "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"D:/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue")),
-  // path: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/vuepress-starter/docs/.vuepress/.temp/pages/404.html.vue")),
   // path: /about/
   "v-74bc627b": defineAsyncComponent(() => import(/* webpackChunkName: "v-74bc627b" */"D:/vuepress-starter/docs/.vuepress/.temp/pages/about/index.html.vue")),
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/vuepress-starter/docs/.vuepress/.temp/pages/404.html.vue")),
 }
--- docs/.vuepress/.temp/internal/searchIndex.js
+++ docs/.vuepress/.temp/internal/searchIndex.js
@@ -1,8 +1,54 @@
 export const searchIndex = [
   {
     "title": "",
-    "headers": [],
+    "headers": [
+      {
+        "level": 3,
+        "title": "有空再补充吧",
+        "slug": "有空再补充吧",
+        "children": []
+      }
+    ],
     "path": "/home.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "",
+    "headers": [
+      {
+        "level": 3,
+        "title": "关于我的经历",
+        "slug": "关于我的经历",
+        "children": [
+          {
+            "level": 4,
+            "title": "简单介绍",
+            "slug": "简单介绍",
+            "children": []
+          },
+          {
+            "level": 4,
+            "title": "家乡",
+            "slug": "家乡",
+            "children": []
+          },
+          {
+            "level": 4,
+            "title": "山楂",
+            "slug": "山楂",
+            "children": []
+          },
+          {
+            "level": 4,
+            "title": "为什么要选择前端",
+            "slug": "为什么要选择前端",
+            "children": []
+          }
+        ]
+      }
+    ],
+    "path": "/about/",
     "pathLocale": "/",
     "extraFields": []
   },
